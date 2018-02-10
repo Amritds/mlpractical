b3cad7d0f5a31a61094cfff214312bb380c73631
Mihaela Stoian <s1453274@sms.ed.ac.uk>
2018-02-10 19:26:40 +0000

    sh scripts added

--- G75m_1.sh
+++ G75m_1.sh
@@ -30,4 +30,4 @@ export TMP=/disk/scratch/${STUDENT_ID}/
 
 source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
 
-python cifar100_network_trainer.py --batch_size 128 --epochs 100 --experiment_prefix base1LeakyReluAdam --dropout_rate 0.4 --batch_norm_use True --strided_dim_reduction False --seed 25012018 --classifier_type Baseline_classifier
+python adam_leaky.py --batch_size 128 --epochs 100 --experiment_prefix base1LeakyReluAdam --dropout_rate 0.4 --batch_norm_use True --strided_dim_reduction False --seed 25012018 --classifier_type Baseline_classifier
